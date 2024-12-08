139e245f1c41e7ecc99c5253ef059b4bf8e7d42e
Akshay Sinha <alt.akshay.sinha@gmail.com>
2024-12-08 18:43:11 +0530

    Variables in js 2

--- 02/script.js
+++ 02/script.js
@@ -1,6 +1,15 @@
+// Preffered way of using variables is camelCase
 var age = 20;
 
 var num1 = 10;
 var num2 = 20;
 
 console.log(num1 + num2);
+
+// Var -> Global Scope
+//means if else ke andar var declared hai to bhi globally accessible ho jayega
+if(true){
+    var name = "Akshay Sinha";
+}
+
+console.log(name);
