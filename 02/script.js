569b0ce4a26429c1249b4c5b2adb655d21dab530
Akshay Sinha <alt.akshay.sinha@gmail.com>
2024-12-08 19:12:03 +0530

    Variables in js 3

--- 02/script.js
+++ 02/script.js
@@ -6,10 +6,34 @@ var num2 = 20;
 
 console.log(num1 + num2);
 
-// Var -> Global Scope
+// var -> Global Scope
 //means if else ke andar var declared hai to bhi globally accessible ho jayega
 if(true){
     var name = "Akshay Sinha";
 }
 
 console.log(name);
+
+//let is local
+let Age = 20;
+
+Age = "Akshay";
+
+console.log(Age);
+
+if (true) {
+    let a = 1;
+}
+
+//console.log(a); //threw error
+
+// let came in new js versions, use let because bugs are less than var
+
+//const -> local
+//const once assigned cannot be changed
+
+const subject = "Maths";
+
+// subject = "Science"; //threw error
+
+console.log(subject);
