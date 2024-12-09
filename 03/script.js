eec3516ef4afcd7242b81d968fb12ec2da043138
Akshay Sinha <alt.akshay.sinha@gmail.com>
2024-12-09 23:14:56 +0530

    03 continued

--- 03/script.js
+++ 03/script.js
@@ -10,4 +10,19 @@ console.log(num1 + num2);
 const firstName = "Akshay";
 const lastName = "Sinha";
 
-console.log(firstName + lastName);
+console.log(firstName + " " + lastName);
+
+//Booleans
+ let isLoggedIn = true
+ console.log(isLoggedIn)
+
+ //Null
+let lastLoginDate = null;
+lastLoginDate = 10; //can be updated later , it takes space
+console.log(lastLoginDate)
+
+//Undefined
+//It does not take sapce it is not existant
+
+let randomString = undefined;
+console.log(randomString)
