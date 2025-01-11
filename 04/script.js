90684017dfa1abfdb816beba5a14bcfaca44190f
Akshay Sinha <alt.akshay.sinha@gmail.com>
2025-01-11 20:16:57 +0530

    04 continued

--- 04/script.js
+++ 04/script.js
@@ -1,8 +1,22 @@
 //Condition Statements
 
-const age = 20;
+const age = 1;
+
 if (age >= 18){
     console.log('you can vote');
-} else{
+} else if(age == 0){
+    console.log('you are a new born, drink milk and chill no need to vote')
+}
+else{
     console.log('no you cant vote')
 }
+
+//Ternary operator (same thing but way easier that huge lines of code)
+//we write condition (? is for if) and then print (: for else statement) and then print statement
+
+age >=18 ? console.log('you are adult') : console.log('minor');
+
+//one more way 
+
+let result = age>=18 ? 'you are Adult' : 'Minor';
+console.log(result)
