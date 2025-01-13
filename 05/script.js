f6d773b138c8a0ba22f0580f3f30ec32f30138bc
Akshay Sinha <alt.akshay.sinha@gmail.com>
2025-01-13 22:50:38 +0530

    05 continued

--- 05/script.js
+++ 05/script.js
@@ -1,9 +1,21 @@
 //Logical Operators
-//AND
+//AND (All conditons must be true &&)
 
 const age = 18;
 const gender = 'male';
 
 if(age>=18  && gender == 'male'){
     console.log('You are an adult male')
+}
+
+//OR (|| atleast one condition must be true)
+if(age>=19 || gender=='male'){
+    console.log('You adult boy')
+}
+
+//NOT
+const number = 5;
+
+if(number % 2 != 0){
+    console.log('Odd')
 }
