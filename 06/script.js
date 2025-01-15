de5425769f77a65b0aee09fdec15fbcc8b0fc096
Akshay Sinha <alt.akshay.sinha@gmail.com>
2025-01-15 23:54:30 +0530

    06 continued

--- 06/script.js
+++ 06/script.js
@@ -16,3 +16,10 @@ while(initial_pos != 5){
     console.log("Step taken untill reached home" + i);
 }
 
+//Do while -> runs code first then checks the code.
+// change initial_pos to 5 to see.
+
+do{
+    initial_pos = initial_pos + 1;
+    console.log("Step taken untill reached home" + i);
+} while(initial_pos <= house_pos)
