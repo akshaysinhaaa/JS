6f0fc5dac58bd23b4de0e922666d273219dd26b3
Akshay Sinha <alt.akshay.sinha@gmail.com>
2025-01-15 23:51:24 +0530

    06 continued

--- 06/script.js
+++ 06/script.js
@@ -4,3 +4,15 @@
 for(let i = 1; i<=10; i = i+1){
     console.log('Akshay' + i);
 }
+
+
+//While Loop
+
+let initial_pos = 0;
+let house_pos = 5;
+
+while(initial_pos != 5){
+    initial_pos = initial_pos + 1;
+    console.log("Step taken untill reached home" + i);
+}
+
