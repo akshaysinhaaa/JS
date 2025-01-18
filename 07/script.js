ed09ba7d9cd63aed9391277ab18fed8747aeafbf
Akshay Sinha <alt.akshay.sinha@gmail.com>
2025-01-19 00:00:39 +0530

    07 continued

--- 07/script.js
+++ 07/script.js
@@ -23,3 +23,15 @@ function multiply(a, b){
 let a = multiply(10, 9);
 console.log('The multiplication is' , a);
 
+//make a function that takes any number of argument
+
+function addNumbers(){
+    let ans = 0
+    for(let i = 0; i < arguments.length; i = i+1){
+        ans = ans + arguments[i]
+    }
+    return ans
+}
+
+let results = addNumbers(1,2,3,4,5)
+console.log(results)
