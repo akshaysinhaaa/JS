017bfef1ad426382d54481c35e706dfbd4d58033
Akshay Sinha <alt.akshay.sinha@gmail.com>
2025-01-18 23:49:05 +0530

    07 continued

--- 07/script.js
+++ 07/script.js
@@ -5,3 +5,21 @@ function sayHello() {
 }
 
 sayHello()
+
+
+// argument
+function add(a, b){
+    console.log(a+b);
+}
+
+add(5, 10)//parameters
+add(9, 7)
+add(8, 11)
+
+function multiply(a, b){
+    return a*b;
+}
+
+let a = multiply(10, 9);
+console.log('The multiplication is' , a);
+
