17ea2169b4686a84a0e438a8beb3ef18896266f1
Akshay Sinha <alt.akshay.sinha@gmail.com>
2025-01-20 22:44:15 +0530

    09 continued

--- 09/script.js
+++ 09/script.js
@@ -8,3 +8,17 @@ function add (a,b, cb){ //let cb is callback
 add(2,4, function (value){ //in parameter of cb we passed a function
     console.log(value);
 });
+
+//also, we can make function in a function in high order functions
+function add (a,b, cb){ 
+    let result = a+b;
+    cb(result);
+
+    return () => console.log(result);
+}
+
+let resultFunction = add(9, 10, () => {}); //nothing's gonna print as cb has empty function
+resultFunction();
+
+
+//Used In API's
