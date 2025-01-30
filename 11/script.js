1d253851af97a22bd47f062c7de811d48e290ab0
Akshay <akshay@Akshays-MacBook-Air-2.local>
2025-01-30 23:18:11 +0530

    11 continued

--- 11/script.js
+++ 11/script.js
@@ -4,3 +4,10 @@ const myArr = ['akshay', 'piyush', 'hello'];
 console.log(myArr);
 
 //forEach
+//-> what it does is it passes your function as an argument
+
+function Hello(n){
+    console.log(n);
+}
+
+myArr.forEach(Hello) //Like a for loop
